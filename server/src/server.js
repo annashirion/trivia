060ef3905cdb82ebf798a2a717b9461d083d9a9a
Anna Shirion <anna.shirion@gmail.com>
2025-10-17 14:35:45 +0300

    Remove unused code

--- server/src/server.js
+++ server/src/server.js
@@ -8,8 +8,6 @@ const cors = require('cors');
 const { getQuestions } = require('./controllers/questionsController');
 const { getTopics } = require('./controllers/topicsController');
 const { checkAnswer } = require('./controllers/answersController');
-const { generateFeedbackAudio } = require('./controllers/feedbackController');
-const { generateGeneralAudio } = require('./controllers/audioController');
 
 const app = express();
 
@@ -19,8 +17,6 @@ app.use(express.json());
 app.get('/api/questions', getQuestions);
 app.get('/api/topics', getTopics);
 app.post('/api/check-answer', checkAnswer);
-app.post('/api/feedback-audio', generateFeedbackAudio);
-app.post('/api/general-audio', generateGeneralAudio);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
